74485355e946317c606bf2b16f85c84195688ebf
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-10-14 09:10:01 +0200

    Adding a link to the INSTALL file

--- lib/Web/Solid/Auth.pm
+++ lib/Web/Solid/Auth.pm
@@ -686,6 +686,11 @@ Web::Solid::Auth - A Perl Solid Web Client
         #..do you curl..lwp::agent..or what ever with the headers
     }
 
+=head1 INSTALLATION
+
+See the L<https://metacpan.org/dist/Web-Solid-Auth/source/INSTALL> file in the 
+distribution.
+
 =head1 DESCRIPTION
 
 This is a Solid-OIDC implementation of a connection class for the Solid
